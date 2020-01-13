d5ec48b91f086246099264d2d23860a259ea0f83
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-01-13 09:24:12 -0600

    save puzzle action reducer

--- word-search/src/store/actions/puzzleAction.js
+++ word-search/src/store/actions/puzzleAction.js
@@ -1,5 +1,25 @@
+import axios from "axios";
+
 export const START_PUZZLE = "START_PUZZLE";
 
 export const generatePuzzle = puzzle => dispatch => {
   dispatch({ type: START_PUZZLE, payload: puzzle });
+};
+
+export const START_SAVE = "START_SAVE";
+export const SAVE_SUCCCESS = "SAVE_SUCCESS";
+export const SAVE_FAILURE = "SAVE_FAILURE";
+
+export const savePuzzle = puzzle => dispatch => {
+  dispatch({ type: START_SAVE });
+
+  axios
+    .post("localhost:5000/api/puzzle", puzzle)
+    .then(res => {
+      return dispatch({ type: SAVE_SUCCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: SAVE_FAILURE, payload: err.response.data.message });
+    });
 };
--- word-search/src/store/reducers/puzzleReducer.js
+++ word-search/src/store/reducers/puzzleReducer.js
@@ -1,9 +1,16 @@
-import { START_PUZZLE } from "../actions/puzzleAction";
+import { START_PUZZLE, SAVE_FAILURE } from "../actions/puzzleAction";
 
 const initState = {
   title: "",
   words: [],
-  size: 10
+  size: 10,
+
+  savePuzzleName: "",
+  savePuzzleCode: "",
+  savePuzzleDescription: "",
+  savePuzzleWords: [],
+  isSaving: false,
+  errorMessage: ""
 };
 
 export const puzzleReducer = (state = initState, action) => {
@@ -13,12 +20,38 @@ export const puzzleReducer = (state = initState, action) => {
         ...state,
         title: action.payload.title,
         words: action.payload.words,
-        size: action.payload.size
+        size: action.payload.size,
+        errorMessage: ""
+      };
+
+    case START_SAVE:
+      return {
+        ...state,
+        isSaving: true,
+        errorMessage: ""
+      };
+
+    case SAVE_SUCCESS:
+      return {
+        ...state,
+        isSaving: false,
+        savePuzzleName: action.payload.puzzle.name,
+        savePuzzleCode: action.payload.puzzle.name,
+        savePuzzleDescription: action.payload.puzzle.name,
+        savePuzzleWords: action.payload.words,
+        errorMessage: ""
+      };
+
+    case SAVE_FAILURE:
+      return {
+        isSaving: false,
+        errorMessage: action.payload
       };
 
     default:
       return {
-        ...state
+        ...state,
+        errorMessage: ""
       };
   }
 };
