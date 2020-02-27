9b298b2bc0fa58dd02c76d84d99e31817cb2a0d2
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-02-26 18:33:44 -0600

    add WorldRecord js file

---
+++ word-search/src/components/WorldRecord/WorldRecord.js
@@ -0,0 +1,12 @@
+import React from "react";
+import puzzle from "../../styles/puzzle.module.scss";
+
+const WorldRecord = () => {
+  return (
+    <div className={puzzle.background}>
+      <p>World Record</p>
+    </div>
+  );
+};
+
+export default WorldRecord;
