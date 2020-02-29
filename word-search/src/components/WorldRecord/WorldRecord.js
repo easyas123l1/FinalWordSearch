e4fbeb9868596617ec1093b53266a1c37f9023a3
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-02-28 23:34:48 -0600

    add logic to bring in letters.txt the worldRecord

--- word-search/src/components/WorldRecord/WorldRecord.js
+++ word-search/src/components/WorldRecord/WorldRecord.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import puzzle from "../../styles/puzzle.module.scss";
 
 const WorldRecord = () => {
+  const [letters, setLetters] = useState([]);
+
+  useEffect(() => {
+    if (letters.length === 0) {
+      const file = require("./letters.txt");
+      let rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, false);
+      rawFile.onreadystatechange = () => {
+        if (rawFile.readyState === 4) {
+          if (rawFile.status === 200 || rawFile.status === 0) {
+            let allText = rawFile.responseText;
+            let splitLetters = allText.split(" ");
+            let bigArray = [];
+            let littleArray = [];
+            for (let i = 0; i < splitLetters.length; i++) {
+              littleArray.push(splitLetters[i]);
+              if (littleArray.length === 500) {
+                bigArray.push(littleArray);
+                littleArray = [];
+              }
+            }
+            console.log(bigArray);
+            setLetters(bigArray);
+          }
+        }
+      };
+      rawFile.send(null);
+    }
+  });
   return (
     <div className={puzzle.background}>
       <p>World Record</p>
