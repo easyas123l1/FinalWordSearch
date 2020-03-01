9334cdcdf561d04f7463a5f6e7e52b06e049762c
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-01 01:18:18 -0600

    create WorldRecordWord.js

---
+++ word-search/src/components/WorldRecord/WorldRecordWords.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import puzzle from "../../styles/puzzle.module.scss";
+
+const WorldRecordWords = () => {
+  return <div></div>;
+};
+
+export default WorldRecordWords;
