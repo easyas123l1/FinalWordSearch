5cc6da9a9b761556328b74304e7209dce3c19781
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-02 11:33:41 -0600

    add LandingPage.js with some placeholder text

---
+++ word-search/src/components/Landing/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import puzzle from "../../styles/puzzle.module.scss";
+
+const LandingPage = () => {
+  return (
+    <div className={puzzle.background}>
+      <p>Landing Page</p>
+    </div>
+  );
+};
+
+export default LandingPage;
