00bb75d3ed636f4787690f25d108d3a21ed66eee
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-03 09:28:42 -0600

    updated some landing page text

--- word-search/src/components/Landing/LandingPage.js
+++ word-search/src/components/Landing/LandingPage.js
@@ -4,16 +4,25 @@ import puzzle from "../../styles/puzzle.module.scss";
 const LandingPage = () => {
   return (
     <div className={puzzle.background}>
-      <p>Landing Page</p>
+      <p>
+        Hello, my name is Andrew Wilson and this is my word search
+        generator/puzzle. I have also generated a static puzzle that currently
+        holds the world record 500x500 with 14,995 words! Currently i'm a full
+        stack web developer student. I have a huge passion for coding!
+      </p>
       <p>
         This website is still under construction. In the mean time feel free to
         create an account to gain access to creating puzzles! Also view the
-        world record puzzle! Thank you for viewing my website!{" "}
+        world record puzzle! Thank you for viewing my website!
       </p>
       <img
         src="https://www.nicepng.com/png/detail/66-667631_website-under-construction-website-still-under-construction.png"
         alt="Website Under Construction - Website Still Under Construction@nicepng.com"
       ></img>
+      <p>
+        email andrew.wilson90@hotmail.com for any advice or if you're looking to
+        hire a full stack web developer!
+      </p>
     </div>
   );
 };
