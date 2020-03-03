45cf2d33208436cd1e0620a122c7dcbef8f28d6f
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-03 09:22:19 -0600

    website still under construction

--- word-search/src/components/Landing/LandingPage.js
+++ word-search/src/components/Landing/LandingPage.js
@@ -5,6 +5,15 @@ const LandingPage = () => {
   return (
     <div className={puzzle.background}>
       <p>Landing Page</p>
+      <p>
+        This website is still under construction. In the mean time feel free to
+        create an account to gain access to creating puzzles! Also view the
+        world record puzzle! Thank you for viewing my website!{" "}
+      </p>
+      <img
+        src="https://www.nicepng.com/png/detail/66-667631_website-under-construction-website-still-under-construction.png"
+        alt="Website Under Construction - Website Still Under Construction@nicepng.com"
+      ></img>
     </div>
   );
 };
