9322daf20c9301a73645d4c3022dde7ac2ef83aa
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-07 22:48:56 -0600

    add name of puzzle and time completed to victory

--- word-search/src/components/Puzzle/Victory.js
+++ word-search/src/components/Puzzle/Victory.js
@@ -2,16 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import puzzle from "../../styles/puzzle.module.scss";
 
-const Victory = () => {
+const Victory = ({ name, time }) => {
   return (
     <div>
-      <p>Completed puzzle page!</p>
+      <p>
+        Completed puzzle {name} in {time}
+      </p>
     </div>
   );
 };
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    name: state.puzzleReducer.playPuzzleName,
+    time: state.puzzleReducer.playPuzzleTime
+  };
 }
 
 export default connect(mapStateToProps, {})(Victory);
