98a639f77ff51cb4f623f934d05de66f8322488c
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-14 17:07:51 -0500

    add footer to page

--- word-search/src/components/Puzzle/AllPuzzles.js
+++ word-search/src/components/Puzzle/AllPuzzles.js
@@ -4,6 +4,7 @@ import { getPuzzles } from "../../store/actions/puzzleAction";
 import PuzzleCard from "./PuzzleCard";
 import puzzle from "../../styles/puzzle.module.scss";
 import { Circle } from "react-spinners-css";
+import Footer from "../Footer/Footer";
 
 const AllPuzzles = ({ getPuzzles, puzzles }) => {
   useEffect(() => {
@@ -14,19 +15,25 @@ const AllPuzzles = ({ getPuzzles, puzzles }) => {
 
   if (puzzles.length === 0) {
     return (
-      <div className={puzzle.background}>
-        <h1>Retrieving puzzles!</h1>
-        <Circle color="blue" size={200} />
+      <div className={puzzle.spacer}>
+        <div className={puzzle.background}>
+          <h1>Retrieving puzzles!</h1>
+          <Circle color="blue" size={200} />
+        </div>
+        <Footer />
       </div>
     );
   } else {
     return (
-      <div className={puzzle.background}>
-        <div className={puzzle.allPuzzle}>
-          {puzzles.map((puzzle) => {
-            return <PuzzleCard key={puzzle.id} puzzle={puzzle} />;
-          })}
+      <div className={puzzle.spacer}>
+        <div className={puzzle.background}>
+          <div className={puzzle.allPuzzle}>
+            {puzzles.map((puzzle) => {
+              return <PuzzleCard key={puzzle.id} puzzle={puzzle} />;
+            })}
+          </div>
         </div>
+        <Footer />
       </div>
     );
   }
