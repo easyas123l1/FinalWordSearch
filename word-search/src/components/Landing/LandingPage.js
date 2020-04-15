d92d57b4bc393ffe24d2c4381ba81f0fb2470f7e
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-14 21:55:37 -0500

    new header image remove website under construction

--- word-search/src/components/Landing/LandingPage.js
+++ word-search/src/components/Landing/LandingPage.js
@@ -6,6 +6,7 @@ const LandingPage = () => {
   return (
     <div className={puzzle.spacer}>
       <div className={puzzle.background}>
+        <img className={puzzle.image} src="books.jpg" alt="Word Search Logo" />
         <p>
           Hello, my name is Andrew Wilson and this is my word search
           generator/puzzle. I have also generated a static puzzle that currently
@@ -17,11 +18,6 @@ const LandingPage = () => {
           to create an account to gain access to creating puzzles! Also view the
           world record puzzle! Thank you for viewing my website!
         </p>
-        <img
-          className={puzzle.image}
-          src="https://www.nicepng.com/png/detail/66-667631_website-under-construction-website-still-under-construction.png"
-          alt="Website Under Construction - Website Still Under Construction@nicepng.com"
-        ></img>
         <p>
           email andrew.wilson90@hotmail.com for any advice or if you're looking
           to hire a full stack web developer!
