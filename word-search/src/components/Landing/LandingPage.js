8f2a715ca9ec44df569d14eff0cb598b09b81b70
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-15 15:47:35 -0500

    html and styling for features

--- word-search/src/components/Landing/LandingPage.js
+++ word-search/src/components/Landing/LandingPage.js
@@ -7,21 +7,61 @@ const LandingPage = () => {
     <div className={puzzle.spacer}>
       <div className={puzzle.background}>
         <img className={puzzle.image} src="books.jpg" alt="Word Search Logo" />
-        <p>
-          Hello, my name is Andrew Wilson and this is my word search
-          generator/puzzle. I have also generated a static puzzle that currently
-          holds the world record 500x500 with 14,994 words! Currently i'm a full
-          stack web developer student. I have a huge passion for coding!
-        </p>
-        <p>
-          This website is still under construction. In the mean time feel free
-          to create an account to gain access to creating puzzles! Also view the
-          world record puzzle! Thank you for viewing my website!
-        </p>
-        <p>
-          email andrew.wilson90@hotmail.com for any advice or if you're looking
-          to hire a full stack web developer!
-        </p>
+        <div className={puzzle.landingPage}>
+          <div className={puzzle.features}>
+            <h1>Create Puzzles With Our Puzzle Generator</h1>
+            <div className={puzzle.featureDescription}>
+              <p>
+                <a href="/register">Register</a> an account with us and create
+                your own custom word search puzzle!
+              </p>
+              <p>
+                With our language filter safely let students create their own
+                word search with peace of mind knowing it will be school
+                friendly!
+              </p>
+              <p>Save your puzzle and share it with your friends and family!</p>
+            </div>
+          </div>
+          <div className={puzzle.features}>
+            <h1>Play Puzzles You & Others Create</h1>
+            <div className={puzzle.featureDescription}>
+              <p>
+                Play from our user created puzzles{" "}
+                <a href="/allPuzzles">view puzzles</a>! All of our puzzles are
+                child friendly!
+              </p>
+              <p>
+                Print out any puzzle to be done away from the computer or in the
+                classroom!
+              </p>
+              <p>
+                Play against classmates to see who can get the fastest time!!
+              </p>
+            </div>
+          </div>
+          <div className={puzzle.features}>
+            <h1>World Record Puzzle</h1>
+            <div className={puzzle.featureDescription}>
+              <p>
+                Creating and solving puzzles is a ton of fun, but if you're
+                looking for a real challenge try checking out our World Record
+                Puzzle!
+              </p>
+              <p>
+                View our{" "}
+                <a href="/worldRecord">Guinesses World Record Puzzle Here!</a>{" "}
+                The puzzle 500x500 with 14,994 words!
+              </p>
+              <p>
+                View all 14,994 words here{" "}
+                <a href="/worldRecordWords">
+                  Guinesses World Record Words To Find!
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
